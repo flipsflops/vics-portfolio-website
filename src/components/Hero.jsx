77b7835beccbf1782b2c4d306de0be8dd5e79e1f
flipsflops <victoribasco18@gmail.com>
2023-06-23 19:12:24 -0700

    node mod fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { motion } from 'framer-motion';
 
 import { styles } from '../styles';
@@ -7,20 +8,19 @@ const Hero = () => {
 	return (
 		<section className={`relative w-full h-screen mx-auto`}>
 			<div
-				className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
-			>
-				<div className="flex flex-col justify-center items-center mt-5">
-					<div className="w-5 h-5 rounded-full bg-[#915EFF]" />
-					<div className="w-1 sm:h-80 h-40 violet-gradient" />
+				className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}>
+				<div className='flex flex-col justify-center items-center mt-5'>
+					<div className='w-5 h-5 rounded-full bg-[#915EFF]' />
+					<div className='w-1 sm:h-80 h-40 violet-gradient' />
 				</div>
 
 				<div>
 					<h1 className={`${styles.heroHeadText} text-white`}>
-						Hi, I'm <span className="text-[#915EFF]">Victor</span>
+						Hi, I'm <span className='text-[#915EFF]'>Victor</span>
 					</h1>
 					<p className={`${styles.heroSubText} mt-2 text-white-100`}>
 						a Fullstack Software Engineer, focused{' '}
-						<br className="sm:block hidden" />
+						<br className='sm:block hidden' />
 						on effective team-work to deliver products that impact
 					</p>
 				</div>
@@ -28,9 +28,9 @@ const Hero = () => {
 
 			<CoolShape />
 
-			<div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-				<a href="#about">
-					<div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+			<div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
+				<a href='#about'>
+					<div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
 						<motion.div
 							animate={{
 								y: [0, 24, 0],
@@ -40,7 +40,7 @@ const Hero = () => {
 								repeat: Infinity,
 								repeatType: 'loop',
 							}}
-							className="w-3 h-3 rounded-full bg-secondary mb-1"
+							className='w-3 h-3 rounded-full bg-secondary mb-1'
 						/>
 					</div>
 				</a>
--- src/main.js
+++
@@ -1,9 +0,0 @@
-define(["react", "react-dom/client", "./App", "./index.css"], function (_react, _client, _App, _index) {
-  "use strict";
-
-  _react = _interopRequireDefault(_react);
-  _client = _interopRequireDefault(_client);
-  _App = _interopRequireDefault(_App);
-  function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
-  _client["default"].createRoot(document.getElementById('root')).render( /*#__PURE__*/_react["default"].createElement(_react["default"].StrictMode, null, /*#__PURE__*/_react["default"].createElement(_App["default"], null)));
-});
