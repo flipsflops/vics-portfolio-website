f1a9ca874b5431ae28d7d7e1d403eba629ada251
flipsflops <victoribasco18@gmail.com>
2023-06-23 19:43:16 -0700

    fixed dist static file references

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	build: {
-		base: '/vics-portfolio-website/',
-	},
+	base: './',
 });
